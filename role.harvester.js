9b26e1cf8c91f13bff2f704a7e5606321be39fc0
Edwan Vi <tkdberger@gmail.com>
2016-08-15 13:16:48 -0400

    Add the tower to structures harvesters can give to

--- role.harvester.js
+++ role.harvester.js
@@ -23,7 +23,7 @@ module.exports = {
             // try to transfer energy, if the spawn is not in range
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
                             structure.energy < structure.energyCapacity;
                     }
             });
