5dad1bfe8da04c47a458ce3182ea5a51e8cb01d1
Edwan Vi <tkdberger@gmail.com>
2016-08-15 13:47:45 -0400

    add logic for remembering flags

--- role.miner.js
+++ role.miner.js
@@ -13,10 +13,15 @@ module.exports = {
 		// second, filter out all that don't start with 'miningFlag'
 		// third, memorize the flag that matches our role.
 		// ten = magic number that means flags only 
-		var flags = creep.room.find(10);
 		if (creep.memory.miner_number == null) {
 			var miner_number = creep.memory.role.substring(5, 6);
 			creep.memory.miner_number = miner_number;
+		}
+		// now we need to find our flag
+		if (creep.memory.miner_number = 1) {
+			creep.memory.flag = Game.flags.miningFlag1;
+		} else {
+			creep.memory.flag = Game.flags.miningFlag2;
 		}
 	}
 };
