4d43f67380122be9e5bc150d35abc59f1162b2f7
Edwan Vi <tkdberger@gmail.com>
2016-09-27 10:53:41 -0400

    Make remote miners act more like harvesters

--- role.remoteminer.js
+++ role.remoteminer.js
@@ -25,6 +25,19 @@ module.exports = {
 			// take energy back to spawn, we're full!
 			if(creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 				creep.moveTo(Game.spawns.Spawn1, {reusePath:50});
+			} else if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_FULL) {
+				// if the spawn if full, take it somewhere else.
+				var targets = creep.room.find(FIND_STRUCTURES, {
+	        filter: (structure) => {
+	          return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_TOWER) &&
+	            structure.energy < structure.energyCapacity;
+	          }
+	        });
+	      if(targets.length > 0) {
+	        if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+	          creep.moveTo(targets[0], {reusePath:50});
+	      	}
+				}
 			}
 		}
 	}
