abe258bf1f4220f21575c161d8e892936c5b3adf
Edwan Vi <tkdberger@gmail.com>
2016-09-27 16:33:02 -0400

    Cleanup and path reuse

--- role.upgrader.js
+++ role.upgrader.js
@@ -12,18 +12,13 @@ module.exports = {
       creep.memory.working = true;
     }
 
-    if(creep.memory.chosensource == null) {
-      //creep.memory.chosensource = creep.pos.findClosestByPath(FIND_SOURCES).id;
-    }
-
     // if creep is supposed to transfer energy to the controller
     if (creep.memory.working == true) {
       // instead of upgraderController we could also use `if (creep.transfer(creep.room.controller, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)`
-
       // try to upgrade the controller
       if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
         // if not in range, move towards the controller
-        creep.moveTo(creep.room.controller, {reusePath:20});
+        creep.moveTo(creep.room.controller, {reusePath:50});
       }
     }
     // if creep is supposed to harvest energy from source
@@ -34,7 +29,7 @@ module.exports = {
       // try to harvest energy, if the source is not in range
       if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
         // move towards the source
-        creep.moveTo(source, {reusePath:20});
+        creep.moveTo(source, {reusePath:50});
       }
     }
   }
