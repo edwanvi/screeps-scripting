3b62b52c5976a0515222a6118b8dc85dd2b3f69b
Edwan Vi <tkdberger@gmail.com>
2016-09-27 12:54:27 -0400

    RE USE PATHS

--- role.remoteminer.js
+++ role.remoteminer.js
@@ -1,3 +1,5 @@
+const rolejanitor = require('role.janitor');
+
 module.exports = {
 	run: function (creep) {
 	//this role goes to a flag, mines around it, and deposits stuff at spawn.
@@ -10,13 +12,12 @@ module.exports = {
     	// switch state
   		creep.memory.working = true;
   	}
-
-  	// if we need to harvest energy
+		// if we need to harvest energy
   	if (!creep.memory.working) {
 			if (creep.room.name === Game.flags.remoteMining.pos.roomName) {
 				var sources = creep.room.find(FIND_SOURCES);
 	    	if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-	    		creep.moveTo(sources[0]);
+	    		creep.moveTo(sources[0], {reusePath: 50});
 				}
   		} else {
 				creep.moveTo(Game.flags.remoteMining, {reusePath:50});
@@ -37,6 +38,9 @@ module.exports = {
 	        if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 	          creep.moveTo(targets[0], {reusePath:50});
 	      	}
+				} else {
+					// console.log("how the f*ck");
+					rolejanitor.run(creep);
 				}
 			}
 		}
