a01acf9ab5cd745d95eac87471663e880eac8503
Edwan Vi <tkdberger@gmail.com>
2016-09-27 09:34:36 -0400

    Remote mining now works!

--- role.remoteminer.js
+++ role.remoteminer.js
@@ -11,15 +11,21 @@ module.exports = {
   		creep.memory.working = true;
   	}
 
-  	//if we need to harvest energy
-  	if (creep.memory.working) {
-  		creep.moveTo(Game.flags.remoteMining);
+  	// if we need to harvest energy
+  	if (!creep.memory.working) {
 			if (creep.room.name === Game.flags.remoteMining.pos.roomName) {
 				var sources = creep.room.find(FIND_SOURCES);
 	    	if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
 	    		creep.moveTo(sources[0]);
 				}
-  		}
+  		} else {
+				creep.moveTo(Game.flags.remoteMining, {reusePath:50});
+			}
+		} else {
+			// take energy back to spawn, we're full!
+			if(creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(Game.spawns.Spawn1, {reusePath:50});
+			}
 		}
 	}
 };
