d835ceb6176659dc34d4454f5ff717593aeb5c66
Edwan Vi <tkdberger@gmail.com>
2016-09-28 10:57:02 -0400

    Add global.MOVE_TO_OPTS This will be used for harvester pathfinding

--- specs.js
+++ specs.js
@@ -7,9 +7,9 @@
  * mod.thing == 'a thing'; // true
  */
 
-module.exports.harvesterSpecs = [WORK, WORK, WORK, CARRY, MOVE, MOVE];
-module.exports.upgraderSpecs = [WORK, WORK, WORK, CARRY, MOVE, MOVE];
-module.exports.builderSpecs = [WORK, WORK, WORK, CARRY, MOVE, MOVE];
+module.exports.harvesterSpecs = [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
+module.exports.upgraderSpecs = [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
+module.exports.builderSpecs = [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
 module.exports.janitorSpecs = [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
 // gloabl values
 global.REUSE_PATH_TICKS = 50;
