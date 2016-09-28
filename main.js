cc7bf369aab1ae303529cbd16212981e00b69f80
Edwan Vi <tkdberger@gmail.com>
2016-09-28 07:36:24 -0400

    New tower, global numberOfCreeps * new tower ID * Tower now ignores roads and walls + global var numberOfCreeps

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // import modules
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
-var roleJanitor = require('role.janitor');
+const roleHarvester = require('role.harvester');
+const roleUpgrader = require('role.upgrader');
+const roleBuilder = require('role.builder');
+const roleJanitor = require('role.janitor');
 const roleremoteminer = require('role.remoteminer');
-var specs = require('specs');
+const specs = require('specs');
 
 module.exports.loop = function () {
   // check for memory entries of dead creeps by iterating over Memory.creeps
@@ -15,10 +15,12 @@ module.exports.loop = function () {
     }
   }
   // man the tower
-  var tower = Game.getObjectById('57af8439d7470970440fa983');
+  var tower = Game.getObjectById('57eb00a1ec14fa4651855289');
   if (tower) {
     var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
       filter: (structure) => structure.hits < structure.hitsMax
+      && structure.structureType != STRUCTURE_ROAD
+      && structure.structureType != STRUCTURE_WALL
     });
     if(closestDamagedStructure) {
       tower.repair(closestDamagedStructure);
@@ -73,6 +75,8 @@ module.exports.loop = function () {
   var numberOfBuilders = _.sum(Game.creeps, (c) => c.memory.role == 'builder');
   var numberOfJanitors = _.sum(Game.creeps, (c) => c.memory.role == 'janitor');
   var numberOfRemotes = _.sum(Game.creeps, (c) => c.memory.role == 'remoteminer');
+
+  global.numberOfCreeps = numberOfRemotes + numberOfJanitors + numberOfBuilders + numberOfUpgraders + numberOfHarvesters;
 
   var name = undefined;
 
