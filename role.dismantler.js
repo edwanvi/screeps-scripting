e277c1ccc2b7794d5f31935c8afe802924614d3c
Edwan Vi <tkdberger@gmail.com>
2016-09-29 10:30:00 -0400

    Pillage! + New creep role: dismantler  Dismantlers take after attackers, coming into a base and destroying  whatever structures they lay eyes on. They also have a CARRY part to  take their sweet loot home.

---
+++ role.dismantler.js
@@ -0,0 +1,21 @@
+const roleremoteminer = require('role.remoteminer');
+
+module.exports = {
+	run: function (creep) {
+		//this role goes to a flag and lays waste to the base it goes to
+		// THIS IS THE ONLY FILE WHERE USING findClosestByPath IS ACCEPTABLE
+		if (Game.spawns.Spawn1.memory.invading === false) {
+			if (creep.room.name === Game.flags.attack.pos.roomName) {
+				var targets = creep.room.findClosestByPath(FIND_HOSTILE_STRUCTURES);
+	    	if (creep.dismantle(targets) == ERR_NOT_IN_RANGE) {
+	    		creep.moveTo(targets);
+				}
+  		} else {
+				creep.moveTo(Game.flags.attack, {reusePath:50});
+			}
+		} else {
+			// what if we aren't invading?
+			roleremoteminer.run(creep);
+		}
+	}
+};
