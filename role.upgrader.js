e9eeb5bc67849813e6aace594d8466b34228b438
Edwan Vi <tkdberger@gmail.com>
2016-09-30 11:19:57 -0400

    Add `working` value to creeps with no memory This allows me to spawn creeps manually easier

--- role.upgrader.js
+++ role.upgrader.js
@@ -11,10 +11,14 @@ module.exports = {
       // switch state
       creep.memory.working = true;
     }
+    // manually spawned creep w/no memory
+    else if (creep.memory.working == undefined) {
+      creep.memory.working = false;
+    }
 
     // if creep is supposed to transfer energy to the controller
     if (creep.memory.working == true) {
-      // instead of upgraderController we could also use `if (creep.transfer(creep.room.controller, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)`
+      // instead of upgradeController we could also use `if (creep.transfer(creep.room.controller, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)`
       // try to upgrade the controller
       if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
         // if not in range, move towards the controller
