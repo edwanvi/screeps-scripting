770ee61b1d2f247bca50e24eeb942cf727a8c8e3
Edwan Vi <edwan.vi@gmail.com>
2017-12-22 12:22:12 -0500

    Format extrautils.js

--- extrautils.js
+++ extrautils.js
@@ -1,12 +1,12 @@
 var newsource = {
-	run: function(creep) {
-        if(Math.random() > 0.5){
-           	creep.memory.energysource = 1;
+        run: function (creep) {
+                if (Math.random() > 0.5) {
+                        creep.memory.energysource = 1;
+                }
+                else {
+                        creep.memory.energysource = 0;
+                }
+                console.log(creep + ' will harvest source ' + creep.memory.energysource + ' for role ' + creep.memory.role);
         }
-        else {
-           	creep.memory.energysource = 0;
-        }
-        console.log(creep + ' will harvest source ' + creep.memory.energysource + ' for role ' + creep.memory.role);
-	}
 }
 module.exports.newsource = newsource;
