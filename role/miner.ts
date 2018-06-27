f7cb14f0bb88c19690db64df3ae9d8fb1e73473d
Edwan Vi <edwan.vi@gmail.com>
2018-06-27 12:45:21 -0400

    Fix filter function for role/miner

--- role/miner.ts
+++ role/miner.ts
@@ -1,7 +1,11 @@
 export class RoleMiner {
     public static run(creep: Creep) {
-        let source = <Source> Game.getObjectById(creep.memory["sourceId"]);
-        let container = source.pos.findInRange(FIND_STRUCTURES, 1, {filter: s => s.structureType == STRUCTURE_CONTAINER})[0];
+        var source = <Source> Game.getObjectById(creep.memory["sourceId"]);
+        let container = source.pos.findInRange(FIND_STRUCTURES, 1, {
+            filter: function(s: AnyStructure) {
+                return s.structureType == STRUCTURE_CONTAINER;
+            }
+        })[0];
         if (creep.pos.isEqualTo(container.pos)) {
             creep.harvest(source);
         } else {
