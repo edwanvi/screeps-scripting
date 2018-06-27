acec5c7698bbbd42c3376fe6ffd707cd8f7d5a4f
Edwan Vi <edwan.vi@gmail.com>
2018-06-27 12:45:57 -0400

    Trucks compile and only deposit in "resting" towers

--- role/truck.ts
+++ role/truck.ts
@@ -1,8 +1,6 @@
-import { ExtendedCreep } from "creep_extension";
-
 // beep beep lettuce
 export class RoleTruck {
-    public static run(creep: ExtendedCreep) {
+    public static run(creep: Creep) {
         if (creep.memory["working"] == undefined || (creep.memory["working"] && creep.carry.energy == 0)) {
             creep.memory["working"] = false;
         } else if (!creep.memory["working"] && creep.carry.energy == creep.carryCapacity) {
@@ -11,25 +9,32 @@ export class RoleTruck {
 
         if (creep.memory["working"]) {
             var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-                filter: s => ([STRUCTURE_SPAWN, STRUCTURE_EXTENSION, STRUCTURE_TOWER].includes(s.structureType)) && s.energy < s.energyCapacity
+                filter: function(s) {
+                    if (s.structureType == STRUCTURE_TOWER) {
+                        return !s.room.memory["towerWorking"];
+                    }
+                    return (s.structureType == STRUCTURE_EXTENSION ||
+                            s.structureType == STRUCTURE_SPAWN) &&
+                            s.energy < s.energyCapacity;
+                }
             });
 
-            if (structure == undefined) {
+            if (structure == undefined && creep.room.storage != undefined) {
                 structure = creep.room.storage;
             }
             // room.storage is sometimes null too!
             if (structure != undefined) {
                 if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(structure, { reusePath = 20, ignoreCreeps = true });
+                    creep.moveTo(structure, { reusePath: 20, ignoreCreeps: true });
                 }
             }
         } else {
             if (creep.memory["containerId"] == undefined) {
                 RoleTruck.setContainer(creep);
             }
-            let container = Game.getObjectById(creep.memory["containerId"]);
-            
-            if (container == undefined) {
+            var container = <Structure> Game.getObjectById(creep.memory["containerId"]);
+
+            if (container == undefined && creep.room.storage != undefined) {
                 container = creep.room.storage;
             }
 
