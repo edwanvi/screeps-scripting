232f1ed2205b9fad52f2b8877512cbd644d6a8bf
Edwan Vi <edwan.vi@gmail.com>
2018-06-27 12:46:36 -0400

    Improve spawn and tower logic

--- main.ts
+++ main.ts
@@ -5,21 +5,32 @@ import { RoleBuilder } from "role/builder";
 import { RoleJanitor } from "role/janitor";
 import { RemoteMiner } from "role/remoteminer";
 import { RoleDefender } from "role/defender";
+import { ExtendedSpawn } from "extended_spawn";
 import * as specs from "specs";
+import { RoleTruck } from "role/truck";
+import { RoleMiner } from "role/miner";
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(function () {
-    var tower = <StructureTower> Game.getObjectById(Game.spawns["Spawn1"].memory["towerId"]);
+    var tower = <StructureTower>Game.getObjectById(Game.spawns["Spawn1"].memory["towerId"]);
     if (tower) {
-        var damagedStructures = tower.room.find(FIND_STRUCTURES, {
-            filter: function (structure: Structure) {
-                return structure.hits < structure.hitsMax && structure.structureType != STRUCTURE_ROAD && structure.structureType != STRUCTURE_WALL;
+        if (tower.room.memory["towerWorking"] && tower.energy == 0) {
+            tower.room.memory["towerWorking"] = false;
+        } else if (!tower.room.memory["towerWorking"] && tower.energy == tower.energyCapacity) {
+            tower.room.memory["towerWorking"] = true;
+        }
+
+        if (tower.room.memory["towerWorking"]) {
+            var damagedStructures = tower.room.find(FIND_STRUCTURES, {
+                filter: function (structure: Structure) {
+                    return structure.hits < structure.hitsMax && structure.structureType != STRUCTURE_ROAD && structure.structureType != STRUCTURE_WALL;
+                }
+            });
+            if (damagedStructures.length > 0) {
+                damagedStructures.sort((a, b) => a.hits - b.hits);
+                tower.repair(damagedStructures[0]);
             }
-        });
-        if (tower.energy / tower.energyCapacity > 0.5 && (damagedStructures.length > 0)) {
-            damagedStructures.sort((a, b) => a.hits - b.hits);
-            tower.repair(damagedStructures[0]);
         }
 
         var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
@@ -42,80 +53,36 @@ export const loop = ErrorMapper.wrapLoop(function () {
         var creep = Game.creeps[name];
         switch (creep.memory["role"]) {
             case 'harvester':
-            RoleHarvester.run(creep);
-            break;
-        case 'upgrader':
-            RoleUpgrader.run(creep);
-            break;
-        case 'builder':
-            RoleBuilder.run(creep);
-            break;
-        case 'janitor':
-            RoleJanitor.run(creep);
-            break;
-        case 'remoteminer':
-            RemoteMiner.run(creep);
-            break;
-        case 'defense':
-            if (RoleDefender.getHostiles() == undefined) {
-                RoleDefender.init(Game.spawns["Spawn1"].room);
-            }
-            RoleDefender.run(creep);
-            break;
-        default:
-            // TODO: Make a role-less creep
-            RoleUpgrader.run(creep);
+                RoleHarvester.run(creep);
+                break;
+            case 'upgrader':
+                RoleUpgrader.run(creep);
+                break;
+            case 'builder':
+                RoleBuilder.run(creep);
+                break;
+            case 'janitor':
+                RoleJanitor.run(creep);
+                break;
+            case 'remoteminer':
+                RemoteMiner.run(creep);
+                break;
+            case 'defense':
+                if (RoleDefender.getHostiles() == undefined) {
+                    RoleDefender.init(Game.spawns["Spawn1"].room);
+                }
+                RoleDefender.run(creep);
+                break;
+            case 'truck':
+                RoleTruck.run(creep);
+                break;
+            case 'miner':
+                RoleMiner.run(creep);
+                break;
+            default:
+                // TODO: Make a role-less creep
+                RoleUpgrader.run(creep);
         }
     }
-
-    // maxiumum populations
-    var maxiumumNumberOfHarvesters = 5;
-    var maxiumumNumberOfUpgraders = 3;
-    var maxiumumNumberOfBuilders = 4;
-    var maxiumumNumberOfJanitors = 2;
-    var maxiumumNumberOfDefenders = 1;
-    // count the number of creeps alive for each role
-    // _.sum will count the number of properties in Game.creeps filtered by the
-    //  arrow function, which checks for the creep being a harvester
-    var numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory["role"] == 'harvester' ? 1 : 0);
-    var numberOfUpgraders = _.sum(Game.creeps, (c) => c.memory["role"] == 'upgrader' ? 1 : 0);
-    var numberOfBuilders = _.sum(Game.creeps, (c) => c.memory["role"] == 'builder' ? 1 : 0);
-    var numberOfJanitors = _.sum(Game.creeps, (c) => c.memory["role"] == 'janitor' ? 1 : 0);
-    var numberOfDefenders = _.sum(Game.creeps, (c) => c.memory["role"] == 'defense' ? 1 : 0);
-
-    var newName: ScreepsReturnCode;
-    var randEnd = _.random(1, 100);
-    var stringPart: string;
-    var room = Game.spawns["Spawn1"].room;
-
-    if (numberOfHarvesters < maxiumumNumberOfHarvesters) {
-        newName = Game.spawns["Spawn1"].spawnCreep(specs.getWorkerSpecs(room), "harv_" + randEnd,
-            { memory: { role: 'harvester', working: false } });
-        stringPart = "harv_";
-    } else if (numberOfUpgraders < maxiumumNumberOfUpgraders) {
-        newName = Game.spawns["Spawn1"].spawnCreep(specs.getWorkerSpecs(room), "up_" + randEnd,
-            { memory: { role: 'upgrader', working: false } });
-        stringPart = "up_";
-    } else if (numberOfBuilders < maxiumumNumberOfBuilders) {
-        newName = Game.spawns["Spawn1"].spawnCreep(specs.getWorkerSpecs(room), "build_" + randEnd,
-            { memory: { role: 'builder', working: false } });
-        stringPart = "build_";
-    } else if (numberOfJanitors < maxiumumNumberOfJanitors) {
-        newName = Game.spawns["Spawn1"].spawnCreep(specs.getWorkerSpecs(room), "jan_" + randEnd,
-            { memory: { role: 'janitor', working: false } });
-        stringPart = "jan_";
-    } else if (numberOfDefenders < maxiumumNumberOfDefenders && Game.spawns["Spawn1"].room.find(FIND_HOSTILE_CREEPS).length > 0) {
-        newName = Game.spawns["Spawn1"].spawnCreep([TOUGH, TOUGH, MOVE, WORK, CARRY, ATTACK], "def_" + randEnd,
-            { memory: { role: 'defense' }});
-        stringPart = "def_";
-    } else {
-        newName = -1;
-        stringPart = "";
-        randEnd = 0;
-    }
-    // print name to console if spawning was a success
-    // name > 0 would not work since string > 0 returns false
-    if (!(newName < 0) && newName != undefined) {
-        console.log("Spawned new creep: " + stringPart + randEnd);
-    }
+    ExtendedSpawn.spawnCreepsAsNeeded(Game.spawns["Spawn1"]);
 });
