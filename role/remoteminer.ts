0630b9dc3dbaad48978ae7baed9d8f0ba5d627c0
Edwan Vi <edwan.vi@gmail.com>
2018-06-27 23:41:13 -0400

    Redo remoteminer "homing" logic

--- role/remoteminer.ts
+++ role/remoteminer.ts
@@ -1,44 +1,54 @@
-export class RemoteMiner {
-  public static run(creep: Creep) {
-    //this role goes to a flag, mines around it, and deposits stuff at spawn.
-    if (creep.memory["working"] && creep.carry.energy == 0) {
-      // switch state
-      creep.memory["working"] = false;
-    }
-    // if creep is harvesting energy but is full
-    else if (!creep.memory["working"] && creep.carry.energy == creep.carryCapacity) {
-      // switch state
-      creep.memory["working"] = true;
-    }
+import { RoleTruck } from "./truck";
 
-    // if we need to harvest energy
-    if (!creep.memory["working"]) {
-      if (creep.room.name === Game.flags["remoteMining"].pos.roomName) {
-        var sources = creep.room.find(FIND_SOURCES);
-        if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(sources[0]);
+export class RemoteMiner {
+    public static run(creep: Creep) {
+        // this role goes to a flag, mines around it, and deposits stuff at spawn.
+        if (creep.memory["working"] && creep.carry.energy == 0) {
+            // switch state
+            creep.memory["working"] = false;
         }
-      } else {
-        creep.moveTo(Game.flags["remoteMining"], { reusePath: 50 });
-      }
-    } else {
-      // take energy back to spawn, we're full!
-      if (creep.transfer(Game.spawns["Spawn1"], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(Game.spawns["Spawn1"], { reusePath: 50 });
-      } else if (creep.transfer(Game.spawns["Spawn1"], RESOURCE_ENERGY) == ERR_FULL) {
-        // if the spawn if full, take it somewhere else.
-        var targets = creep.room.find(FIND_STRUCTURES, {
-          filter: (structure) => {
-            return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_TOWER) &&
-              structure.energy < structure.energyCapacity;
-          }
-        });
-        if (targets.length > 0) {
-          if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(targets[0], { reusePath: 50 });
-          }
+        // if creep is harvesting energy but is full
+        else if (!creep.memory["working"] && creep.carry.energy == creep.carryCapacity) {
+            // switch state
+            creep.memory["working"] = true;
         }
-      }
+
+        // if we need to harvest energy
+        if (!creep.memory["working"]) {
+            if (creep.room.name === Game.flags["remoteMining"].pos.roomName) {
+                var sources = creep.room.find(FIND_SOURCES);
+                if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[0]);
+                }
+            } else {
+                creep.moveTo(Game.flags["remoteMining"], { reusePath: 50 });
+            }
+        } else {
+            // take energy back to spawn, we're full!
+            if (creep.room.name == creep.memory["homeRoom"]) {
+                var structure = creep.room.find(FIND_MY_STRUCTURES, {
+                    filter: function (s) {
+                        if (s.structureType == STRUCTURE_TOWER) {
+                            return !s.room.memory["towerWorking"];
+                        }
+                        return (s.structureType == STRUCTURE_EXTENSION ||
+                            s.structureType == STRUCTURE_SPAWN) &&
+                            s.energy < s.energyCapacity;
+                    }
+                })[0];
+
+                if (structure == undefined && creep.room.storage != undefined) {
+                    structure = creep.room.storage;
+                }
+                // room.storage is sometimes null too!
+                if (structure != undefined) {
+                    if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(structure, { reusePath: 20, ignoreCreeps: true });
+                    }
+                }
+            } else {
+                creep.moveTo(new RoomPosition(25, 25, creep.memory["homeRoom"]));
+            }
+        }
     }
-  }
 };
