46c489842f8c2aa15686a9b0c1f4c0058476046b
Edwan Vi <edwan.vi@gmail.com>
2018-06-27 23:09:54 -0400

    Change ExtendedCreep to be wrapper method + respect memory source

--- creep_extension.ts
+++ creep_extension.ts
@@ -1,32 +1,38 @@
-export class ExtendedCreep extends Creep {
-    public getEnergy(useContainers: boolean, useSources: boolean) {
+import { ExUt } from "extrautils";
+
+export class ExtendedCreep {
+    public static getEnergy(creep: Creep, useContainers: boolean, useSources: boolean) {
         let container;
         // if the Creep should look for containers
         if (useContainers) {
             // find closest container
-            container = this.pos.findClosestByPath(FIND_STRUCTURES, {
+            container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: s => (s.structureType == STRUCTURE_CONTAINER || s.structureType == STRUCTURE_STORAGE) &&
-                             s.store[RESOURCE_ENERGY] > 0
+                    s.store[RESOURCE_ENERGY] > 0
             });
             // if one was found
             if (container != undefined) {
                 // try to withdraw energy, if the container is not in range
-                if (this.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     // move towards it
-                    this.moveTo(container);
+                    creep.moveTo(container);
                 }
             }
         }
         // if no container was found and the Creep should look for Sources
         // TODO: Respect source in memory
         if (container == undefined && useSources) {
-            // find closest source
-            var source = this.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+            // find memory source
+            if (creep.memory["energysource"] == null) {
+                ExUt.newSource(creep);
+            }
+
+            var source = creep.room.find(FIND_SOURCES)[creep.memory["energysource"]];
 
             // try to harvest energy, if the source is not in range
-            if (this.harvest(source) == ERR_NOT_IN_RANGE) {
+            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 // move towards it
-                this.moveTo(source);
+                creep.moveTo(source);
             }
         }
     }
