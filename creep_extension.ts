e97295cb549d0883b54a2e774504f64a0dee4b5b
Edwan Vi <edwan.vi@gmail.com>
2018-06-28 07:22:07 -0400

    Prefer storage to regular containers

--- creep_extension.ts
+++ creep_extension.ts
@@ -6,10 +6,15 @@ export class ExtendedCreep {
         // if the Creep should look for containers
         if (useContainers) {
             // find closest container
-            container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: s => (s.structureType == STRUCTURE_CONTAINER || s.structureType == STRUCTURE_STORAGE) &&
-                    s.store[RESOURCE_ENERGY] > 0
-            });
+            if (creep.room.storage != undefined) {
+                container = creep.room.storage.store[RESOURCE_ENERGY] >= creep.carryCapacity ? creep.room.storage : undefined;
+            }
+            if (container == undefined) {
+                container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter: s => (s.structureType == STRUCTURE_CONTAINER) &&
+                        s.store[RESOURCE_ENERGY] > 0
+                });
+            }
             // if one was found
             if (container != undefined) {
                 // try to withdraw energy, if the container is not in range
@@ -32,7 +37,7 @@ export class ExtendedCreep {
             // try to harvest energy, if the source is not in range
             if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 // move towards it
-                creep.moveTo(source);
+                creep.moveTo(source, { reusePath: 50, ignoreCreeps: true });
             }
         }
     }
